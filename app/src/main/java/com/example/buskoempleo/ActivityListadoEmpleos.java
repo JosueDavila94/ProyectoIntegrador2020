bcbbc7aba5f701b221f9f51ff507ff5af448e2fe
JosueDavila <josuedaviladiaz@gmail.com>
2020-05-31 23:56:31 -0500

    Se creó segundo activity

---
+++ app/src/main/java/com/example/buskoempleo/ActivityListadoEmpleos.java
@@ -0,0 +1,13 @@
+package com.example.buskoempleo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityListadoEmpleos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado_empleos);
+    }
+}
